564f169ce65e9492c2803c4a87277d9249763c5f
ethan <ethaneshom@gmail.com>
2025-09-03 15:33:50 -0700

    scanner stuff

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-
-
-
+//import scanner to use for user input
+import java.util.Scanner;
 
 public class Main{
 
     public static void main(String[] args) {
+
+        //create a new scanner
+        Scanner scanner = Scanner(System.in);
 
 
     }
