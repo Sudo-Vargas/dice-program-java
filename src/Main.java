3657af720bc2ca27c8c1b0bc47ed94401a32db55
ethan <ethaneshom@gmail.com>
2025-09-03 15:44:29 -0700

    dice sides and number variables/scanner input

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,15 @@ public class Main{
     public static void main(String[] args) {
 
         //create a new scanner
-        Scanner scanner = Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        //gets number of dice from user and puts it in a variable called dicenum
+        System.out.println("How many dice would you like to roll?");
+        int dicenum = scanner.nextInt();
+
+        //gets number of dice sides from user and puts it in a variable dicesides
+        System.out.println("How many sides should they have?");
+        int dicesides = scanner.nextInt();
 
 
     }
