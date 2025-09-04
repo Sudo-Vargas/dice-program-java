2cba37dc93adf5d19e6bfaf053327ff148a71816
ethan <ethaneshom@gmail.com>
2025-09-04 08:12:44 -0700

    made array, total variable, for loop, sum of dice rolls.

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,33 @@ public class Main{
         System.out.println("How many sides should they have?");
         int dicesides = scanner.nextInt();
 
+        //create variable length array with dicenum as highest number
+        int[] rolls = new int[dicenum];
+
+        //create total variable to start counting total
+        int total = 0;
+
+        // for loop to roll dice dicenum times
+        for (int i = 0; i < dicenum; i++) {
+
+            //generate random roll from 1 to dicesides
+            int roll = (int) (Math.random() * dicesides) + 1;
+
+            //store roll value in the array
+            rolls[i] = roll;
+
+            //add roll to total
+            total = total + roll;
+
+            //print dice roll
+            System.out.println("Die " + (i +1) + ": " + rolls[i]);
+        }
+
+        //print sum of dice rolls
+        System.out.println("Sum of all rolls: " + total);
+
+        //close scanner
+        scanner.close();
 
     }
 
